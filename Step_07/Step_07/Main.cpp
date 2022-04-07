71dd37bfd89a4a21b26f631dc6dc0f7771f0d803
Eunpyeong Jeon <siroyacha@gmail.com>
2022-04-07 20:52:10 +0900

    Step_08

--- Step_07/Step_07/Main.cpp
+++ Step_07/Step_07/Main.cpp
@@ -2,5 +2,67 @@
 
 int main()
 {
+	/*
+	int iArrayA[5] = { 1,2,3,4,5 };
+	int iArrayB[] = { 1,2,3,4,5 };
+	int iArrayC[5] = { 1,2 };
+	int iArrayD[5] = { 0 };
 
+	for (int i = 0; i < 5; i++)
+	{
+		printf_s("%d¹ø-[A : %d] [B : %d] [C : %d] [D : %d]\n\n",
+			i, iArrayA[i], iArrayB[i], iArrayC[i], iArrayD[i]);
+	}
+	*/
+	
+	/*
+	int iArray[3][3] =
+	{
+		{1,2,3},
+		{4,5,6},
+		{7,8,9},
+	};
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			printf_s("%d\t", iArray[i][j]);
+
+			if (j==2)
+			{
+				printf_s("\n");
+			}
+		}
+	}
+	printf_s("\n");
+	*/
+
+	/*
+	char srt1[5] = "Good";
+
+	for (int i = 0; i < 4; i++)
+	{
+		printf_s("%c", srt1[i]);
+	}
+	*/
+	
+	/*
+	char str1[5] = { 'G','o','o','d','\0'};
+	char str2[] = { 'M','o','r','n','i','n','g','!','\0'};
+	char str3[] = "Sunshine";
+	char str4[] = { 'G','o','o','d', 32, 'M','o','r','n','i','n','g','!', 0};
+
+
+	for (int i = 0; i < 5; i++)
+	{
+		printf_s("%c", str1[i]);
+	}
+
+	printf_s("%s %s\n", str2, str3);
+	printf_s("\n");
+	printf_s("%s\n", str4);
+	*/
+
+	return 0;
 }

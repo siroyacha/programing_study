ef88befbc8d0a22bec090d5de24dcaa23d5f8214
Eunpyeong Jeon <siroyacha@gmail.com>
2022-04-08 20:52:38 +0900

    Step_08

--- Step_08/Step_08/Main.cpp
+++ Step_08/Step_08/Main.cpp
@@ -2,11 +2,38 @@
 
 int main()
 {
+	/*
+	int iNumber1 = 10;
+	int iNumber2 = 100;
 
+	printf_s("iNumber1 : %d\n", iNumber1);
+	printf_s("iNumber2 : %d\n", iNumber2);
+
+	printf_s("iNumber1 林家蔼: %d\n", &iNumber1);
+	printf_s("iNumber2 林家蔼: %d\n", &iNumber2);
+	*/
 	
+	/*
+	int iNumber1 = 10;
+	int iNumber2 = 100;
 
+	int* pPointer = &iNumber2;
 
+	printf_s("pPointer狼 蔼 : %d\n", *pPointer);
+	*/
 
+	/*
+	int iNumber = 19;
+	int* piNumber;
+	piNumber = &iNumber;
+
+	printf_s("iNumber   : %d\n", iNumber);
+	printf_s("*piNumber : %d\n", *piNumber);
+	printf_s("piNumber  : %d\n", piNumber);
+	printf_s("&iNumber  : %d\n", &iNumber);
+	printf_s("&piNumber : %d\n\n", &piNumber);
+	*/
+	
 
 	return 0;		 
 }

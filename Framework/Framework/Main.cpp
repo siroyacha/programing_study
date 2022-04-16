128b223391a3cc4a656ab1470bad23209317e9be
Eunpyeong Jeon <siroyacha@gmail.com>
2022-04-17 02:22:15 +0900

    Framewrok v0.3

--- Framework/Framework/Main.cpp
+++ Framework/Framework/Main.cpp
@@ -8,11 +8,12 @@
 
 // ** 월요일 전까지 프레임워크 만들면서 한줄한줄 주석처리 하면서 만들기
 
-
+//플레이어와 몬스터를 구분 하는 용도의 변수 선언
 const int PLAYER = 0;
 const int ENEMY	 = 1;
 const int Max	 = 2;
 
+//플레이어와 몬스터 등 캐릭터들의 정보를 구성하는 구조체
 typedef struct tagInfo
 {
 	// ** 스탯 타입은 재량(int, float 상관 없음)
@@ -35,23 +36,26 @@ typedef struct tagObject
 
 }OBJECT;
 
+//필요한 함수들의 전방선언
 void InitializeObject(OBJECT* _Obj, int ObjectType);
 char* SetName();
 
 int main()
 {
+	//플레이어와 몬스터의 정보 구조체를 선언
 	OBJECT Player;
 	OBJECT Enemy;
 
 	OBJECT* Objects[Max];
 
+	//플레이어와 몬스터를 동적 할당
 	Objects[PLAYER] = (OBJECT*)malloc(sizeof(OBJECT));
 	InitializeObject(Objects[PLAYER], PLAYER);
 
 	Objects[ENEMY] = (OBJECT*)malloc(sizeof(OBJECT));
 	InitializeObject(Objects[ENEMY], ENEMY);
 
-
+	//상태창 출력을 하는 반복문
 	while (true)
 	{
 		// ** 콘솔창을 모두 지움.
@@ -65,8 +69,6 @@ int main()
 		printf_s("EXP : %d\n", Objects[PLAYER]->Info.EXP);
 		printf_s("Level : %d\n\n", Objects[PLAYER]->Info.Level);
 
-		Sleep(500);
-
 		printf_s("[%s]\n", Objects[ENEMY]->Info.Name);
 		printf_s("HP : %d\n", Objects[ENEMY]->Info.HP);
 		printf_s("MP : %d\n", Objects[ENEMY]->Info.MP);
@@ -75,75 +77,102 @@ int main()
 		printf_s("EXP : %d\n", Objects[ENEMY]->Info.EXP);
 		printf_s("Level : %d\n\n", Objects[ENEMY]->Info.Level);
 
+		//출력을 지연시키는 함수
 		Sleep(500);
 
+		//공격과 도망을 선택하는 선택지를 받을 변수
 		int iChoice = 0;
+
+		//도망 체크를 위한 값을 입력받을 변수
 		int run = 0;
+
 		printf_s("몬스터와 만났습니다. 공격하시겠습니까 ?\n1. 공격\n2. 도망\n입력 : ");
 		scanf_s("%d", &iChoice);
 
+		//플레이어의 선택으로 공격과 도망을 수행하는 스위치문
 		switch (iChoice)
 		{
+			//공격 행동을 실행하는 부분
 		case 1:
+			//플레이어의 공격을 실행하는 부분
 			printf("[%s]의 공격\n", Objects[PLAYER]->Info.Name);
+			//플레이어의 공격력이 몬스터의 방어력보다 높은경우
 			if (Objects[PLAYER]->Info.Att > Objects[ENEMY]->Info.Def)
 			{
 				Objects[ENEMY]->Info.HP -= Objects[PLAYER]->Info.Att - Objects[ENEMY]->Info.Def;
 			}
+			//플레이어의 공격력이 몬스터의 방어력보다 낮은 경우
 			else
 				Objects[ENEMY]->Info.HP -= 1;
 
-			Sleep(500);
-
+			//몬스터의 공격을 실행하는 부분
 			printf("[%s]의 공격\n", Objects[ENEMY]->Info.Name);
+			//몬스터의 공격력이 플레이어의 방어력보다 높은 경우
 			if (Objects[ENEMY]->Info.Att > Objects[PLAYER]->Info.Def)
 			{
 				Objects[PLAYER]->Info.HP -= Objects[ENEMY]->Info.Att - Objects[PLAYER]->Info.Def;
 			}
+			//몬스터의 공격력이 플레이어의 방어력보다 낮은 경우
 			else
 				Objects[PLAYER]->Info.HP -= 1;
 
 			break;
+
+			//도망 행동을 실행하는 부분
 		case 2:
+			//랜덤 시드 초기화
 			srand(time(NULL));
+			//랜덤함수를 통한 확률 결정
 			run = rand();
+			//몬스터의 레벨이 플레이어보다 높을 경우 플레이어에게 패널티를 주어 도망 확률을 낮추는 과정
 			if (Objects[ENEMY]->Info.Level> Objects[PLAYER]->Info.Level)
 			{
+				//패널티를 감안하여 도주 확률을 결정하고 성공한 경우
 				if (run % (Objects[ENEMY]->Info.Level - Objects[PLAYER]->Info.Level) +1 == 1)
 				{
 					printf_s("도망치는것에 [성공] 했습니다.\n");
-					iChoice++;
+					//도망치는데 성공하여 씬이 끝나는 부분
+					break;
 				}
+				//도망에 실패했을 경우
 				else
 				{
 					printf_s("도망치는것에 [실패] 했습니다.\n");
+					//다음 씬 출력전 지연을 주는 부분
 					Sleep(500);
 				}
 			}
+			//플레이어의 레벨이 몬스터보다 높은 경우
 			else
+				//50프로의 확률로 도망에 성공
 				if (run % 2 == 1)
 				{
 					printf_s("도망치는것에 [성공] 했습니다.\n");
-					iChoice++;
+					//도망치는데 성공하여 씬이 끝나는 부분
+					break;
 				}
+				//도망에 실패했을 경우
 				else
 				{
 					printf_s("도망치는것에 [실패] 했습니다.\n");
+					//다음 씬 출력전 지연을 주는 부분
 					Sleep(500);
-				}			
+				}
+			//도망 부분이 끝나고 돌아가는 부분
 			break;
 		}
 
-		if (iChoice == 3)
-			break;
 	}
 	return 0;
 }
 
+//캐릭터들의 정보를 초기화하는 함수
 void InitializeObject(OBJECT* _Obj, int ObjectType)
 {
+	//스위치문을 통해 캐릭터 타입별로 다른 정보로 초기화
 	switch (ObjectType)
 	{
+		//플레이어로 정보 초기화
 	case PLAYER:
 		_Obj->Info.Name = SetName();
 
@@ -154,6 +183,7 @@ void InitializeObject(OBJECT* _Obj, int ObjectType)
 		_Obj->Info.MP = 10;
 		_Obj->Info.Level = 1;
 		break;
+		//몬스터로 정보 초기화
 	case ENEMY:
 		_Obj->Info.Name = (char*)"Enemy";
 
@@ -166,16 +196,20 @@ void InitializeObject(OBJECT* _Obj, int ObjectType)
 		break;
 	}
 }
-
+//이름을 입력받는 함수
 char* SetName()
 {
+	//이름을 입력받을 변수
 	char Buffer[128] = "";
 
 	printf_s("이름 입력 : ");
 	scanf("%s", Buffer);
 
+	//입력받은 이름을 동적할당으로 저장할 변수(null값을 포함하기 때문에 크기가 1이 더 크다)
 	char* pName = (char*)malloc(strlen(Buffer) + 1);
+	//문자열 복사로 이름을 저장
 	strcpy(pName, Buffer);
 
+	//이름 반환
 	return pName;
 }

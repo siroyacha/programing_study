bc014ab35a821d746184bd0249c38ebd5cba4184
Eunpyeong Jeon <siroyacha@gmail.com>
2022-04-26 19:24:59 +0900

    Framework v0.6

--- Framework/Framework/Main.cpp
+++ Framework/Framework/Main.cpp
@@ -11,13 +11,14 @@ const int PLAYER = 0;
 const int ENEMY	 = 1;
 const int Max	 = 2;
 
-
 const int Scene_Logo = 0;
 const int Scene_Menu = 1;
 const int Scene_Stage = 2;
 const int Scene_Exit = 3;
 
 int SceneState = 0;
+
+int Check = 1;
 
 //플레이어와 몬스터 등 캐릭터들의 정보를 구성하는 구조체
 typedef struct tagInfo
@@ -39,18 +40,29 @@ typedef struct tagInfo
 // ** 오브젝트 단위로 묶기 위한 구조체
 typedef struct tagObject
 {
+	char* Name;
 	INFO Info;
 
 }OBJECT;
 
 //필요한 함수들의 전방선언
-void SceneManager();
-void InitializeObject(OBJECT* _Obj, int ObjectType);
+
+void SceneManager(OBJECT* _Player, OBJECT* _Enemy);
 char* SetName();
 
-void StageScene(OBJECT* Player, OBJECT* Enemy);
 void LogoScene();
 void MenuScene();
+void StageScene(OBJECT* _Player, OBJECT* _Enemy);
+
+void InitializePlayer(OBJECT* _Player);
+void PlayerScene(OBJECT* _Player);
+
+void InitializeEnemy(OBJECT* _Enemy);
+void EnemyScene(OBJECT* _Enemy);
+
+void SetPosition(int _x, int _y, char* _str, int _Color=15);
+void SetColor(int _Color);
+void HideCursor();
 
 int main()
 {
@@ -73,8 +85,24 @@ int main()
 	StageScene(Objects[PLAYER], Objects[ENEMY]);
 	*/
 
-	DWORD dwTime = GetTickCount();
-	int Delay = 0;
+
+	// ** 커서를 안보이게 함
+	HideCursor();
+
+	// ** 콘솔창 사이즈 설정
+	system("mode con:cols=120 lines=30");
+
+	//**콘솔창 이름 설정
+	system("title 전은평 Framework v0.6");
+
+	OBJECT* Player = (OBJECT*)malloc(sizeof(OBJECT));
+	InitializePlayer(Player);
+
+	OBJECT* Monster = (OBJECT*)malloc(sizeof(OBJECT));
+	InitializeEnemy(Monster);
+
+	DWORD dwTime = GetTickCount(); // 1/1000 (1000분의 1초)
+	int Delay = 1000;
 
 	int UpCount = 0;
 
@@ -83,16 +111,76 @@ int main()
 		if (dwTime + Delay < GetTickCount())
 		{
 			dwTime = GetTickCount();
-
 			system("cls");
 
+			printf_s("%s\n", Player->Name);
 
+			// ** 게임 루프
+			SceneManager(Player, Monster);
 		}
 	}
+
 	return 0;
 }
 
+void SceneManager(OBJECT* _Player, OBJECT* _Enemy)
+{
+	switch (SceneState)
+	{
+	case Scene_Logo:
+		LogoScene();
+		break;
+	case Scene_Menu:
+		MenuScene();
+		break;
+	case Scene_Stage:
+		StageScene(_Player, _Enemy);
+		break;
+	case Scene_Exit:
+
+		exit(NULL);// ** 프로그램 종료
+		break;
+	}
+}
+
+void LogoScene()
+{
+	int Width = (120 / 2) - (strlen("      ,gggg,     _,gggggg,_        ,gg,         _,gggggg,_      ") / 2);
+	int Height = 10;
+
+	SetPosition(Width, Height + 1, (char*)"      ,gggg,     _,gggggg,_        ,gg,         _,gggggg,_      ");
+	SetPosition(Width, Height + 2, (char*)"      d8` `8I   ,d8P``d8P`Y8b,     i8``8i      ,d8P``d8P`Y8b,   ");
+	SetPosition(Width, Height + 3, (char*)"      88  ,dP  ,d8'   Y8   `8b,dP  `8,,8'     ,d8'   Y8   `8b,dP");
+	SetPosition(Width, Height + 4, (char*)"   8888888P`   d8'    `Ybaaad88P'   `Y88aaad8 d8'    `Ybaaad88P'");
+	SetPosition(Width, Height + 5, (char*)"      88       8P       `````Y8      d8````Y8,8P       `````Y8  ");
+	SetPosition(Width, Height + 6, (char*)"      88       8b            d8     ,8P     8b8b            d8  ");
+	SetPosition(Width, Height + 7, (char*)" ,aa,_88       Y8,          ,8P     dP      Y8Y8,          ,8P  ");
+	SetPosition(Width, Height + 8, (char*)"dP` `88P       `Y8,        ,8P' _ ,dP'      I8`Y8,        ,8P'  ");
+	SetPosition(Width, Height + 9, (char*)"Yb,_,d88b,,_    `Y8b,,__,,d8P'  `888,,_____,dP `Y8b,,__,,d8P'   ");
+	SetPosition(Width, Height + 10, (char*)" `Y8P`  `Y88888   ``Y8888P`'    a8P`Y888888P`    ``Y8888P`'     ");
+
+	Sleep(5000);
+	SceneState++;
+}
+
+void MenuScene()
+{
+	printf_s("MenuScene\n");
+
+	printf_s("다음 씬 ㄱㄱ??\n1. 이동\n2. 종료\n일력 : ");
+
+	int i = 0;
+	scanf("%d", &i);
+
+
+	if (i == 1)
+		SceneState++;
+	else if (i == 2)
+		SceneState = Scene_Exit;
+}
+
 //캐릭터들의 정보를 초기화하는 함수
+/*
 void InitializeObject(OBJECT* _Obj, int ObjectType)
 {
 	//스위치문을 통해 캐릭터 타입별로 다른 정보로 초기화
@@ -122,6 +210,55 @@ void InitializeObject(OBJECT* _Obj, int ObjectType)
 		break;
 	}
 }
+*/
+
+
+
+void InitializePlayer(OBJECT* _Player)
+{
+	_Player->Name = SetName();
+
+	_Player->Info.Att = 10;
+	_Player->Info.Def = 10;
+	_Player->Info.EXP = 0;
+	_Player->Info.HP = 100;
+	_Player->Info.MP = 10;
+	_Player->Info.Level = 1;
+}
+
+DWORD SetnameTime = 0;
+
+void PlayerScene(OBJECT* _Player)
+{
+	if (SetnameTime + 10000 < GetTickCount())
+		Check = 1;
+
+	if (Check)
+	{
+		SetnameTime = GetTickCount();
+
+		_Player->Name = SetName();
+		Check = 0;
+	}
+}
+
+void InitializeEnemy(OBJECT* _Enemy)
+{
+	_Enemy->Name = (char*)"Enemy";
+
+	_Enemy->Info.Att = 5;
+	_Enemy->Info.Def = 15;
+	_Enemy->Info.EXP = 0;
+	_Enemy->Info.HP = 30;
+	_Enemy->Info.MP = 5;
+	_Enemy->Info.Level = 1;
+}
+
+void EnemyScene(OBJECT* _Enemy)
+{
+
+}
+
 
 //이름을 입력받는 함수
 char* SetName()
@@ -142,8 +279,14 @@ char* SetName()
 }
 
 //스테이지 신을 출력하는 함수
-void StageScene(OBJECT* Player, OBJECT* Enemy)
+void StageScene(OBJECT* _Player, OBJECT* _Enemy)
 {
+	// ** 전투
+
+	PlayerScene(_Player);
+	EnemyScene(_Enemy);
+
+	/*
 	//루프문 체크를 할 변수
 	int LoopCheck = 1;
 
@@ -259,4 +402,40 @@ void StageScene(OBJECT* Player, OBJECT* Enemy)
 			break;
 		}
 	}
+	*/
 }
+
+void SetPosition(int _x, int _y, char* _str, int _Color)
+{
+	COORD Pos = { _x,_y };
+
+	SetConsoleCursorPosition( GetStdHandle(STD_OUTPUT_HANDLE), Pos);
+	SetColor(_Color);
+
+	printf_s("%s", _str);
+}
+
+void SetColor(int _Color)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), _Color);
+}
+
+void HideCursor()
+{
+	CONSOLE_CURSOR_INFO Info;
+
+	Info.dwSize = 1;
+	Info.bVisible = false;
+
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &Info);
+}
+
+/*
+	__             ______
+   / /     ____   / ____ / ___
+  / /     / __ \ / /   __ / __ \
+ / /___  / /_/  / /  _/ //_/ /  |
+/_____ / \____ / \____ / \____ /
+
+
+*/
